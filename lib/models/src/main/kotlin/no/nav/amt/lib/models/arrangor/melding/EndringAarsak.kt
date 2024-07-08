188006ed1bbebec903919c28bf94d5bc0ac4d25d
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2024-07-08 11:50:06 +0200

    Legg årsaker inne i interface for å samle de sammen (#14)

--- lib/models/src/main/kotlin/no/nav/amt/lib/models/arrangor/melding/EndringAarsak.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/arrangor/melding/EndringAarsak.kt
@@ -3,20 +3,20 @@ package no.nav.amt.lib.models.arrangor.melding
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.SIMPLE_NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-sealed interface EndringAarsak
+sealed interface EndringAarsak {
+    data object Syk : EndringAarsak
 
-data object Syk : EndringAarsak
+    data object FattJobb : EndringAarsak
 
-data object FattJobb : EndringAarsak
+    data object TrengerAnnenStotte : EndringAarsak
 
-data object TrengerAnnenStotte : EndringAarsak
+    data object FikkIkkePlass : EndringAarsak
 
-data object FikkIkkePlass : EndringAarsak
+    data object Utdanning : EndringAarsak
 
-data object Utdanning : EndringAarsak
+    data object IkkeMott : EndringAarsak
 
-data object IkkeMott : EndringAarsak
-
-data class Annet(
-    val beskrivelse: String,
-) : EndringAarsak
+    data class Annet(
+        val beskrivelse: String,
+    ) : EndringAarsak
+}
