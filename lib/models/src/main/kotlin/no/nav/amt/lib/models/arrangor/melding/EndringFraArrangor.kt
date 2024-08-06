806d7862735e7621ac1a97ad1484a199b37293ff
Tormod Fossum <tormod.fossum@nav.no>
2024-08-06 10:32:26 +0200

    Legg til EndringFraArrangor

---
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/arrangor/melding/EndringFraArrangor.kt
@@ -0,0 +1,21 @@
+package no.nav.amt.lib.models.arrangor.melding
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class EndringFraArrangor(
+    val id: UUID,
+    val deltakerId: UUID,
+    val opprettetAvArrangorAnsattId: UUID,
+    val opprettet: LocalDateTime,
+    val endring: Endring,
+) : Melding {
+    @JsonTypeInfo(use = JsonTypeInfo.Id.SIMPLE_NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+    sealed interface Endring
+
+    data class LeggTilOppstartsdato(
+        val startdato: LocalDateTime,
+        val sluttdato: LocalDateTime?,
+    )
+}
