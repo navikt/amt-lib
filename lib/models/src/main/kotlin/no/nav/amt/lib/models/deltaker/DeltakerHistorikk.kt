68b4f6f6ff1799a37775ada0de8dbe03dad9e4f2
Tia Firing <tia.firing@nav.no>
2024-09-16 09:19:00 +0200

    legger til deltakerhistorikk med tilhørende klasser

---
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/DeltakerHistorikk.kt
@@ -0,0 +1,44 @@
+package no.nav.amt.lib.models.deltaker
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.SIMPLE_NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+sealed class DeltakerHistorikk {
+    val sistEndret
+        get() = when (this) {
+            is Endring -> endring.endret
+            is Vedtak -> vedtak.sistEndret
+            is Forslag -> forslag.sistEndret
+            is EndringFraArrangor -> endringFraArrangor.opprettet
+        }
+
+    data class Endring(
+        val endring: DeltakerEndring,
+    ) : DeltakerHistorikk()
+
+    data class Vedtak(
+        val vedtak: no.nav.amt.lib.models.deltaker.Vedtak,
+    ) : DeltakerHistorikk()
+
+    data class Forslag(
+        val forslag: no.nav.amt.lib.models.arrangor.melding.Forslag,
+    ) : DeltakerHistorikk()
+
+    data class EndringFraArrangor(
+        val endringFraArrangor: no.nav.amt.lib.models.arrangor.melding.EndringFraArrangor,
+    ) : DeltakerHistorikk()
+
+    fun navAnsatte() = when (this) {
+        is Endring -> listOf(this.endring.endretAv)
+        is Vedtak -> listOfNotNull(this.vedtak.sistEndretAv, this.vedtak.opprettetAv)
+        is Forslag -> listOfNotNull(this.forslag.getNavAnsatt()?.id)
+        is EndringFraArrangor -> emptyList()
+    }
+
+    fun navEnheter() = when (this) {
+        is Endring -> listOf(this.endring.endretAvEnhet)
+        is Vedtak -> listOfNotNull(this.vedtak.sistEndretAvEnhet, this.vedtak.opprettetAvEnhet)
+        is Forslag -> listOfNotNull(this.forslag.getNavAnsatt()?.enhetId)
+        is EndringFraArrangor -> emptyList()
+    }
+}
---
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/DeltakerStatus.kt
@@ -0,0 +1,53 @@
+package no.nav.amt.lib.models.deltaker
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class DeltakerStatus(
+    val id: UUID,
+    val type: Type,
+    val aarsak: Aarsak?,
+    val gyldigFra: LocalDateTime,
+    val gyldigTil: LocalDateTime?,
+    val opprettet: LocalDateTime,
+) {
+    data class Aarsak(
+        val type: Type,
+        val beskrivelse: String?,
+    ) {
+        init {
+            if (beskrivelse != null && type != Type.ANNET) {
+                error("Aarsak $type skal ikke ha beskrivelse")
+            }
+        }
+
+        enum class Type {
+            SYK,
+            FATT_JOBB,
+            TRENGER_ANNEN_STOTTE,
+            FIKK_IKKE_PLASS,
+            IKKE_MOTT,
+            ANNET,
+            AVLYST_KONTRAKT,
+            UTDANNING,
+            SAMARBEIDET_MED_ARRANGOREN_ER_AVBRUTT,
+        }
+    }
+
+    enum class Type {
+        KLADD,
+        UTKAST_TIL_PAMELDING,
+        AVBRUTT_UTKAST,
+        VENTER_PA_OPPSTART,
+        DELTAR,
+        HAR_SLUTTET,
+        IKKE_AKTUELL,
+        FEILREGISTRERT,
+        SOKT_INN,
+        VURDERES,
+        VENTELISTE,
+        AVBRUTT,
+        FULLFORT,
+        PABEGYNT_REGISTRERING,
+    }
+}
