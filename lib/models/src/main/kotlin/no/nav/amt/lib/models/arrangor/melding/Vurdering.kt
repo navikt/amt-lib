9f86501854f1c67a5f797422288870aaeab65173
tiafiring <tiafiring@gmail.com>
2025-01-09 06:59:24 +0100

    vurdering fra arrangør (#96)
    * vurdering fra arrangør
    
    * fjerner overflødige felter

---
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/arrangor/melding/Vurdering.kt
@@ -0,0 +1,13 @@
+package no.nav.amt.lib.models.arrangor.melding
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class Vurdering(
+    override val id: UUID,
+    override val deltakerId: UUID,
+    override val opprettetAvArrangorAnsattId: UUID,
+    override val opprettet: LocalDateTime,
+    val vurderingstype: Vurderingstype,
+    val begrunnelse: String?,
+) : Melding
---
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/arrangor/melding/Vurderingstype.kt
@@ -0,0 +1,5 @@
+package no.nav.amt.lib.models.arrangor.melding
+
+enum class Vurderingstype {
+	OPPFYLLER_KRAVENE, OPPFYLLER_IKKE_KRAVENE
+}
