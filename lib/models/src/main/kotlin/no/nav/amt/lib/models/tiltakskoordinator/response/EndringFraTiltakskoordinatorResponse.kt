028cbd13def52aabab5d4782e1a93ecc100ef2bf
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2025-03-17 14:57:29 +0100

    Fjern delt med arrangor (#118)
    * Fjern delt med arrangor fra status
    
    * Bump org.junit.jupiter:junit-jupiter-engine from 5.12.0 to 5.12.1

--- lib/models/src/main/kotlin/no/nav/amt/lib/models/tiltakskoordinator/response/EndringFraTiltakskoordinatorResponse.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/tiltakskoordinator/response/EndringFraTiltakskoordinatorResponse.kt
@@ -1,11 +1,10 @@
 package no.nav.amt.lib.models.tiltakskoordinator.response
 
-import no.nav.amt.lib.models.deltaker.DeltakerStatus
 import java.time.LocalDateTime
 import java.util.UUID
 
 data class EndringFraTiltakskoordinatorResponse(
     val id: UUID,
-    val status: DeltakerStatus,
+    val erDeltManueltMedArrangor: Boolean,
     val sistEndret: LocalDateTime,
 )
