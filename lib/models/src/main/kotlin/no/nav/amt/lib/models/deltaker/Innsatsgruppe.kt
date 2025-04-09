9c963047da324e8a9b20017fb09a08dfa874f3d6
Tormod Fossum <tormod.fossum@nav.no>
2025-04-09 14:21:14 +0200

    Legg til InnsatsgruppeV2

--- lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/Innsatsgruppe.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/Innsatsgruppe.kt
@@ -7,3 +7,23 @@ enum class Innsatsgruppe {
     VARIG_TILPASSET_INNSATS,
     GRADERT_VARIG_TILPASSET_INNSATS,
 }
+
+/**
+ * Dette er innsatsgruppene som vi får fra OBO, og fra Valp (på tiltakstyper) i dag, men vi har ennå ikke migrert over til å bruke disse,
+ * så man må huske å konvertere enumene til første versjon `Innsatsgruppe` foreløpig.
+ */
+enum class InnsatsgruppeV2 {
+    GODE_MULIGHETER,
+    TRENGER_VEILEDNING,
+    TRENGER_VEILEDNING_NEDSATT_ARBEIDSEVNE,
+    JOBBE_DELVIS,
+    LITEN_MULIGHET_TIL_A_JOBBE,
+}
+
+fun InnsatsgruppeV2.toV1() = when (this) {
+    InnsatsgruppeV2.GODE_MULIGHETER -> Innsatsgruppe.STANDARD_INNSATS
+    InnsatsgruppeV2.TRENGER_VEILEDNING -> Innsatsgruppe.SITUASJONSBESTEMT_INNSATS
+    InnsatsgruppeV2.TRENGER_VEILEDNING_NEDSATT_ARBEIDSEVNE -> Innsatsgruppe.SPESIELT_TILPASSET_INNSATS
+    InnsatsgruppeV2.JOBBE_DELVIS -> Innsatsgruppe.GRADERT_VARIG_TILPASSET_INNSATS
+    InnsatsgruppeV2.LITEN_MULIGHET_TIL_A_JOBBE -> Innsatsgruppe.VARIG_TILPASSET_INNSATS
+}
