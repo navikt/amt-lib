ee832e300a9441f78c7f27a445a135c84a87f66a
Tormod Fossum <tormod.fossum@nav.no>
2025-04-09 14:28:29 +0200

    Hjepemetode for å konvertere fra gammel til ny enum

--- lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/Innsatsgruppe.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/Innsatsgruppe.kt
@@ -27,3 +27,11 @@ fun InnsatsgruppeV2.toV1() = when (this) {
     InnsatsgruppeV2.JOBBE_DELVIS -> Innsatsgruppe.GRADERT_VARIG_TILPASSET_INNSATS
     InnsatsgruppeV2.LITEN_MULIGHET_TIL_A_JOBBE -> Innsatsgruppe.VARIG_TILPASSET_INNSATS
 }
+
+fun Innsatsgruppe.toV2() = when (this) {
+    Innsatsgruppe.STANDARD_INNSATS -> InnsatsgruppeV2.GODE_MULIGHETER
+    Innsatsgruppe.SITUASJONSBESTEMT_INNSATS -> InnsatsgruppeV2.TRENGER_VEILEDNING
+    Innsatsgruppe.SPESIELT_TILPASSET_INNSATS -> InnsatsgruppeV2.TRENGER_VEILEDNING_NEDSATT_ARBEIDSEVNE
+    Innsatsgruppe.GRADERT_VARIG_TILPASSET_INNSATS -> InnsatsgruppeV2.JOBBE_DELVIS
+    Innsatsgruppe.VARIG_TILPASSET_INNSATS -> InnsatsgruppeV2.LITEN_MULIGHET_TIL_A_JOBBE
+}
