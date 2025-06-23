f0ff1f588afe415539213e9a65636090e194684f
Tormod Fossum <tormod.fossum@nav.no>
2025-06-23 16:40:42 +0200

    Legg til noe dokumentasjon

--- lib/outbox/src/main/kotlin/no/nav/amt/lib/outbox/OutboxProcessor.kt
+++ lib/outbox/src/main/kotlin/no/nav/amt/lib/outbox/OutboxProcessor.kt
@@ -6,6 +6,19 @@ import no.nav.amt.lib.utils.objectMapper
 import org.slf4j.LoggerFactory
 import java.time.Duration
 
+/**
+ * Processes and publishes outbox events.
+ * The processor is designed to be run as a background job.
+ *
+ * It will periodically fetch unprocessed events from the database and publish them to Kafka.
+ * If an event fails to be published, it will be marked as failed and will be retried later.
+ * To avoid publishing events out of order for the same key, the processor will not publish any new events for a key/topic
+ * if a previous event for the same key/topic has failed.
+ *
+ * @param service The service for interacting with the outbox event repository.
+ * @param jobManager The job manager for scheduling the background job.
+ * @param producer The Kafka producer for publishing events.
+ */
 class OutboxProcessor(
     private val service: OutboxService,
     private val jobManager: JobManager,
@@ -13,6 +26,12 @@ class OutboxProcessor(
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
+    /**
+     * Starts a background job that periodically processes outbox events.
+     *
+     * @param initialDelay The initial delay before the job starts.
+     * @param period The period between each job execution.
+     */
     fun start(initialDelay: Duration = Duration.ofMillis(1000), period: Duration = Duration.ofMillis(5000)) {
         jobManager.startJob(
             name = "outbox-processor",
