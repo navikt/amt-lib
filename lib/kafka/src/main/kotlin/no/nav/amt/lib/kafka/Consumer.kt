b935f76ec03c20850dade85737508a07a2c6e30c
Izis Frøyhov <izis.froyhov@nav.no>
2025-06-30 09:15:07 +0200

    Legger til ny start funksjon, som eksternt skal erstatte run slik at state blir riktig initialisert (#181)

--- lib/kafka/src/main/kotlin/no/nav/amt/lib/kafka/Consumer.kt
+++ lib/kafka/src/main/kotlin/no/nav/amt/lib/kafka/Consumer.kt
@@ -5,5 +5,8 @@ import kotlinx.coroutines.Job
 interface Consumer<K, V> {
     suspend fun consume(key: K, value: V)
 
+    @Deprecated("Use start() instead.", ReplaceWith("start()"))
     fun run(): Job
+
+    fun start()
 }
