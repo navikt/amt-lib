aac7ec66850bf0491c5c1d958da9009bdbe05c00
Izis Frøyhov <izis.froyhov@nav.no>
2025-10-01 11:04:22 +0200

    Flytter ut enumer for tiltakskode og arenatiltakskode for

---
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/tiltakstype/ArenaKode.kt
@@ -0,0 +1,44 @@
+package no.nav.amt.lib.models.deltakerliste.tiltakstype
+
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.AMO
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.ARBEIDSFORBEREDENDE_TRENING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.ARBEIDSRETTET_REHABILITERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.AVKLARING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.DIGITALT_OPPFOLGINGSTILTAK
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.FAG_OG_YRKESOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.GRUPPE_ARBEIDSMARKEDSOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.GRUPPE_FAG_OG_YRKESOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.HOYERE_UTDANNING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.JOBBKLUBB
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.OPPFOLGING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.VARIG_TILRETTELAGT_ARBEID_SKJERMET
+
+enum class ArenaKode {
+    ARBFORB,
+    ARBRRHDAG,
+    AVKLARAG,
+    DIGIOPPARB,
+    INDOPPFAG,
+    GRUFAGYRKE,
+    GRUPPEAMO,
+    JOBBK,
+    VASV,
+    ENKELAMO,
+    ENKFAGYRKE,
+    HOYEREUTD;
+
+    fun toTiltaksKode() = when (this) {
+        ARBFORB -> ARBEIDSFORBEREDENDE_TRENING
+        ARBRRHDAG -> ARBEIDSRETTET_REHABILITERING
+        AVKLARAG -> AVKLARING
+        DIGIOPPARB -> DIGITALT_OPPFOLGINGSTILTAK
+        GRUPPEAMO -> GRUPPE_ARBEIDSMARKEDSOPPLAERING
+        GRUFAGYRKE -> GRUPPE_FAG_OG_YRKESOPPLAERING
+        JOBBK -> JOBBKLUBB
+        INDOPPFAG -> OPPFOLGING
+        VASV -> VARIG_TILRETTELAGT_ARBEID_SKJERMET
+        ENKELAMO -> AMO
+        ENKFAGYRKE -> FAG_OG_YRKESOPPLAERING
+        HOYEREUTD -> HOYERE_UTDANNING
+    }
+}
--- lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/tiltakstype/Tiltakstype.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/tiltakstype/Tiltakstype.kt
@@ -17,55 +17,6 @@ data class Tiltakstype(
         navn
     }
 
-    enum class ArenaKode {
-        ARBFORB,
-        ARBRRHDAG,
-        AVKLARAG,
-        DIGIOPPARB,
-        INDOPPFAG,
-        GRUFAGYRKE,
-        GRUPPEAMO,
-        JOBBK,
-        VASV,
-        ENKELAMO,
-        ENKFAGYRKE,
-        HOYEREUTD
-    }
-
-    enum class Tiltakskode {
-        ARBEIDSFORBEREDENDE_TRENING,
-        ARBEIDSRETTET_REHABILITERING,
-        AVKLARING,
-        DIGITALT_OPPFOLGINGSTILTAK,
-        GRUPPE_ARBEIDSMARKEDSOPPLAERING,
-        GRUPPE_FAG_OG_YRKESOPPLAERING,
-        JOBBKLUBB,
-        OPPFOLGING,
-        VARIG_TILRETTELAGT_ARBEID_SKJERMET,
-        HOYERE_UTDANNING,
-        AMO,
-        FAG_OG_YRKESOPPLAERING,
-        ;
-
-        @Deprecated("Utrygg sjekk av kurstiltak. Må erstattes med å sjekke oppstartstype på tiltak")
-        fun erKurs() = this in kursTiltak
-
-        fun toArenaKode() = when (this) {
-            ARBEIDSFORBEREDENDE_TRENING -> ArenaKode.ARBFORB
-            ARBEIDSRETTET_REHABILITERING -> ArenaKode.ARBRRHDAG
-            AVKLARING -> ArenaKode.AVKLARAG
-            DIGITALT_OPPFOLGINGSTILTAK -> ArenaKode.DIGIOPPARB
-            GRUPPE_ARBEIDSMARKEDSOPPLAERING -> ArenaKode.GRUPPEAMO
-            GRUPPE_FAG_OG_YRKESOPPLAERING -> ArenaKode.GRUFAGYRKE
-            JOBBKLUBB -> ArenaKode.JOBBK
-            OPPFOLGING -> ArenaKode.INDOPPFAG
-            VARIG_TILRETTELAGT_ARBEID_SKJERMET -> ArenaKode.VASV
-            AMO -> ArenaKode.ENKELAMO
-            FAG_OG_YRKESOPPLAERING -> ArenaKode.ENKFAGYRKE
-            HOYERE_UTDANNING -> ArenaKode.HOYEREUTD
-        }
-    }
-
     companion object {
         val kursTiltak = setOf(
             Tiltakskode.GRUPPE_ARBEIDSMARKEDSOPPLAERING,
@@ -80,7 +31,7 @@ data class Tiltakstype(
     }
 
     @Deprecated("Utrygg sjekk av kurstiltak. Må erstattes med å sjekke oppstartstype på tiltak")
-    fun erKurs() = this.tiltakskode.erKurs()
+    fun erKurs() = this.tiltakskode in kursTiltak
 
     fun erEnkeltplass() = this.tiltakskode in enkeltplassTiltak
 
