a0e8cd4e3b3bb678a99b4700c47ec96e0e225f7a
MartheFH <136322563+MartheFH@users.noreply.github.com>
2025-11-14 11:14:38 +0100

    Legge til sluttdato på EndreAvslutningRequest også (#270)

--- lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/internalapis/deltaker/request/EndreAvslutningRequest.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/internalapis/deltaker/request/EndreAvslutningRequest.kt
@@ -1,6 +1,7 @@
 package no.nav.amt.lib.models.deltaker.internalapis.deltaker.request
 
 import no.nav.amt.lib.models.deltaker.DeltakerEndring
+import java.time.LocalDate
 import java.util.UUID
 
 data class EndreAvslutningRequest(
@@ -9,5 +10,6 @@ data class EndreAvslutningRequest(
     override val forslagId: UUID?,
     val aarsak: DeltakerEndring.Aarsak?,
     val begrunnelse: String?,
+    val sluttdato: LocalDate?,
     val harFullfort: Boolean,
 ) : EndringForslagRequest
