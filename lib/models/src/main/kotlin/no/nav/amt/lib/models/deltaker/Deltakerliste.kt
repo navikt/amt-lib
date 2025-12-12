2e0d1281814d1712acd825d065c1efee4fd8f4fd
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-12-12 12:00:47 +0100

    Lagt til Deltakerliste.gjennomforingstype

--- lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/Deltakerliste.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/Deltakerliste.kt
@@ -1,5 +1,6 @@
 package no.nav.amt.lib.models.deltaker
 
+import no.nav.amt.lib.models.deltakerliste.GjennomforingType
 import no.nav.amt.lib.models.deltakerliste.Oppstartstype
 import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltak
 import java.time.LocalDate
@@ -8,6 +9,7 @@ import java.util.UUID
 data class Deltakerliste(
     val id: UUID,
     val navn: String,
+    val gjennomforingstype: GjennomforingType = GjennomforingType.Gruppe, // fjern default-verdi etter neste relast
     val tiltak: Tiltak,
     val startdato: LocalDate? = null,
     val sluttdato: LocalDate? = null,
