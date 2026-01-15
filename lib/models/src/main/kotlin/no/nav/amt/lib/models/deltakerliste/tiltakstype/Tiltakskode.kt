08f435c40d6f9926245ce18b90ee07da57a5467e
Izis Frøyhov <izis.froyhov@nav.no>
2026-01-15 07:28:54 +0100

    Legger inn definisjon av opplæringstilgak som skal brukes for å * bestemme hvilke innholdselementer som skal være påkrevd * Brukes for å bestemme avsluttende statuser * Legger til utfyllende doc om tiltakstyper * Litt usikker på om de gamle enkeltplassene skal bli med for alle behovene men tar med nå siden det er et opplæringstiltak

--- lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/tiltakstype/Tiltakskode.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/tiltakstype/Tiltakskode.kt
@@ -1,27 +1,66 @@
 package no.nav.amt.lib.models.deltakerliste.tiltakstype
 
 enum class Tiltakskode {
+    /*
+        Individuelle tiltak som alltid har løpende oppstart og direktegodkjent
+    */
     ARBEIDSFORBEREDENDE_TRENING,
     ARBEIDSRETTET_REHABILITERING,
     AVKLARING,
-    DIGITALT_OPPFOLGINGSTILTAK,
-    GRUPPE_ARBEIDSMARKEDSOPPLAERING,
-    GRUPPE_FAG_OG_YRKESOPPLAERING,
-    JOBBKLUBB,
     OPPFOLGING,
     VARIG_TILRETTELAGT_ARBEID_SKJERMET,
-    HOYERE_UTDANNING,
+    DIGITALT_OPPFOLGINGSTILTAK, //Digitalt jobbsøkerkurs
+
+    /*
+        Kurstiltak som ofte har oppstartstype felles men kan også i tilfeller ha oppstartstype løpende.
+        Bruk av denne tiltakstypen med oppstartstype løpende kan tyde på at det er arbeidsmarkedsopplæring med rammeavtale.
+        Oppstartstype FELLES skal gi KREVER_GODKJENNING
+        Oppstartstype LØPENDE skal gi DIREKTE_GODKJENT
+     */
+    GRUPPE_ARBEIDSMARKEDSOPPLAERING,
+    GRUPPE_FAG_OG_YRKESOPPLAERING,
+    JOBBKLUBB, // Jobbsøkerkurs
+
+    /*
+        Enkeltplasstiltak som skal fases ut. Kan kun kan registreres i arena.
+        Disse har 1-1 med gjennomføring.
+     */
     ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING,
     ENKELTPLASS_FAG_OG_YRKESOPPLAERING,
-    ARBEIDSMARKEDSOPPLAERING, // Vil etter hvert ta over for GRUPPE_ARBEIDSMARKEDSOPPLAERING og ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING
-    NORSKOPPLAERING_GRUNNLEGGENDE_FERDIGHETER_FOV,
-    STUDIESPESIALISERING,
-    FAG_OG_YRKESOPPLAERING,
-    HOYERE_YRKESFAGLIG_UTDANNING,
+
+    /*
+        Enkeltplasstiltak som potensielt skal videreføres fra arena
+     */
+    HOYERE_UTDANNING,
+
+    /*
+        Tiltak etter ny forskrift som skal erstatte bruk av
+        ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING, ENKELTPLASS_FAG_OG_YRKESOPPLAERING, GRUPPE_ARBEIDSMARKEDSOPPLAERING, GRUPPE_FAG_OG_YRKESOPPLAERING
+        Kan ha oppstartstype Felles/Løpende
+        De med Løpende oppstart kan ha enten DIREKTE_VEDTAK eller KREVER_GODKJENNING
+        De med Felles oppstart har alltid KREVER_GODKJENNING
+     */
+    ARBEIDSMARKEDSOPPLAERING, // Tidligere GruppeAMO/EnkelAMO
+    NORSKOPPLAERING_GRUNNLEGGENDE_FERDIGHETER_FOV, // Tidligere GruppeAMO/EnkelAMO
+    STUDIESPESIALISERING, // Tidligere GruppeAMO/EnkelAMO
+    FAG_OG_YRKESOPPLAERING, // Tidligere GruppeFagYrk/EnkelFagYrk
+    HOYERE_YRKESFAGLIG_UTDANNING, // Tidligere GruppeFagYrk/EnkelFagYrk
     ;
 
     // Ved lansering av ny forskrift/påmelding av nye typer må vi bruke type feltet GRUPPE/ENKELPLASS istedet for tiltakskode
     fun erEnkeltplass() = this in setOf(HOYERE_UTDANNING, ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING, ENKELTPLASS_FAG_OG_YRKESOPPLAERING)
+
+    fun erOpplaeringstiltak() = this in setOf(
+        ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING,
+        ENKELTPLASS_FAG_OG_YRKESOPPLAERING,
+        HOYERE_UTDANNING,
+        GRUPPE_ARBEIDSMARKEDSOPPLAERING,
+        GRUPPE_FAG_OG_YRKESOPPLAERING,
+        ARBEIDSMARKEDSOPPLAERING,
+        NORSKOPPLAERING_GRUNNLEGGENDE_FERDIGHETER_FOV,
+        STUDIESPESIALISERING,
+        FAG_OG_YRKESOPPLAERING,
+    )
 
     @Deprecated("Denne skal antakelig erstattes av lokalt tilpassede versjoner")
     fun toArenaKode() = when (this) {
