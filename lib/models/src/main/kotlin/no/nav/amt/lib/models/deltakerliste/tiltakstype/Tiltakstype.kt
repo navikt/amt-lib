94d11edc7ef32ce0a8ed4f6e8033f7ebe237eb7b
Izis Frøyhov <izis.froyhov@nav.no>
2026-02-17 08:54:41 +0100

    definerer liste over opplæringstiltak som skal brukes for å bestemme hvilke tiltakstyper man kan endre på som tiltaksansvarlig

--- lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/tiltakstype/Tiltakstype.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/tiltakstype/Tiltakstype.kt
@@ -1,6 +1,16 @@
 package no.nav.amt.lib.models.deltakerliste.tiltakstype
 
 import no.nav.amt.lib.models.deltaker.Innsatsgruppe
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.ARBEIDSMARKEDSOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.ENKELTPLASS_FAG_OG_YRKESOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.FAG_OG_YRKESOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.GRUPPE_ARBEIDSMARKEDSOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.GRUPPE_FAG_OG_YRKESOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.HOYERE_UTDANNING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.HOYERE_YRKESFAGLIG_UTDANNING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.NORSKOPPLAERING_GRUNNLEGGENDE_FERDIGHETER_FOV
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.STUDIESPESIALISERING
 import java.util.UUID
 
 data class Tiltakstype(
@@ -18,14 +28,26 @@ data class Tiltakstype(
 
     companion object {
         val kursTiltak = setOf(
-            Tiltakskode.GRUPPE_ARBEIDSMARKEDSOPPLAERING,
-            Tiltakskode.GRUPPE_FAG_OG_YRKESOPPLAERING,
+            GRUPPE_ARBEIDSMARKEDSOPPLAERING,
+            GRUPPE_FAG_OG_YRKESOPPLAERING,
             Tiltakskode.JOBBKLUBB,
         )
         val enkeltplassTiltak = setOf(
-            Tiltakskode.HOYERE_UTDANNING,
-            Tiltakskode.ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING,
-            Tiltakskode.ENKELTPLASS_FAG_OG_YRKESOPPLAERING,
+            HOYERE_UTDANNING,
+            ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING,
+            ENKELTPLASS_FAG_OG_YRKESOPPLAERING,
+        )
+        val opplaeringsTiltak = setOf(
+            ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING,
+            ENKELTPLASS_FAG_OG_YRKESOPPLAERING,
+            HOYERE_UTDANNING,
+            GRUPPE_ARBEIDSMARKEDSOPPLAERING,
+            GRUPPE_FAG_OG_YRKESOPPLAERING,
+            ARBEIDSMARKEDSOPPLAERING,
+            NORSKOPPLAERING_GRUNNLEGGENDE_FERDIGHETER_FOV,
+            STUDIESPESIALISERING,
+            FAG_OG_YRKESOPPLAERING,
+            HOYERE_YRKESFAGLIG_UTDANNING
         )
     }
 
