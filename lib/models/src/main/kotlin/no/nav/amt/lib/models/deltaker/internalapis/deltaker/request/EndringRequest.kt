721427f3e255fa63290fcf35ba1c38bb38d17d86
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-02-20 17:57:20 +0100

    Lagt til JsonTypeInfo på interface EndringRequest

--- lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/internalapis/deltaker/request/EndringRequest.kt
+++ lib/models/src/main/kotlin/no/nav/amt/lib/models/deltaker/internalapis/deltaker/request/EndringRequest.kt
@@ -1,5 +1,8 @@
 package no.nav.amt.lib.models.deltaker.internalapis.deltaker.request
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.SIMPLE_NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 sealed interface EndringRequest {
     val endretAv: String
     val endretAvEnhet: String
---
+++ lib/models/src/test/kotlin/no/nav/amt/lib/models/request/AvsluttDeltakelseRequestTest.kt
@@ -0,0 +1,33 @@
+package no.nav.amt.lib.models.request
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import no.nav.amt.lib.models.deltaker.internalapis.deltaker.request.AvsluttDeltakelseRequest
+import no.nav.amt.lib.utils.objectMapper
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class AvsluttDeltakelseRequestTest {
+    @Test
+    fun getForslagId() {
+        val request = AvsluttDeltakelseRequest(
+            endretAv = "test",
+            endretAvEnhet = "test-enhet",
+            forslagId = null,
+            sluttdato = LocalDate.now(),
+            aarsak = null,
+            begrunnelse = null,
+        )
+
+        val json = objectMapper.writeValueAsString(request)
+
+        json shouldContain
+            """
+            "type" : "AvsluttDeltakelseRequest"
+            """.trimIndent()
+
+        val deserialized = objectMapper.readValue<AvsluttDeltakelseRequest>(json)
+        deserialized shouldBe request
+    }
+}
